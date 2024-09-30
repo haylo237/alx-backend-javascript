cae05dc62a572b594f850f6f319c384a9f6bbe75
haylo237 <njikeabednego@hotmail.com>
2024-09-30 10:52:29 -0700

    task 2

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,4 @@
 import { taskFirst, taskNext } from './0-constants.js';
 import taskBlock from './1-block-scoped.js';
+import getNeighborhoodsList from './2-arrow.js';
 console.log(`${taskFirst()} ${taskNext()}`);
