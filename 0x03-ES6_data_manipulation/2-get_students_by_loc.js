cc30a6fb0b18ea5c8bbaf5571663db3487c1a0a6
Haylo237 <njikeabednego@hotmail.com>
2024-10-09 02:57:46 +0100

    corrected task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,8 @@
 function getStudentsByLocation(students, city) {
-    if (!Array.isArray(students) || typeof city !== 'string') {
-        return [];
-    }
-    return students.filter(student => student.location === city);
+  if (!Array.isArray(students) || typeof city !== 'string') {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
 }
 
 export default getStudentsByLocation;
-
