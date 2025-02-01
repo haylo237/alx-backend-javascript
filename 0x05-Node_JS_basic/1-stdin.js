c181425210a5cd4afb6202d53a9e7b63a218297a
Njike Njiah Abednego <njikeabednego@hotmail.com>
2025-02-01 10:52:21 +0000

    corrected task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-process.stdout.write("Welcome to ALX, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (input) => {
-    const name = input.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.stdin.end();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-process.stdin.on("end", () => {
-    console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
