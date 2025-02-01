e0bc210e5041202af82c55913287a0d8f16e6b39
Njike Njiah Abednego <njikeabednego@hotmail.com>
2025-02-01 10:26:24 +0000

    first

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT
+ * @param {string} msg the message to display
+ * @author Njike Njiah Abednego <https://github.com/haylo237>
+ */
+
+const displayMessage = (msg) => {
+    console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This Directory is for Node JS Basics
